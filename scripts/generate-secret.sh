88c7264ae475000b2418c9fd7dd91f6d8d0f9f98
Code-Aether <aetherraito@protonmail.com>
2025-06-07 19:05:05 -0300

    fix: Correct syntax error and validation of secret
    As this script tries to use openssl or python3 to
    generate a hash, now the script only runs the python
    code if there has a error with openssl
    At the end, checks if any of the two created a secret

--- scripts/generate-secret.sh
+++ scripts/generate-secret.sh
@@ -6,17 +6,19 @@ SECRET=
 if command -v openssl >/dev/null 2>&1; then
     SECRET=$(openssl rand -base64 64 | tr -d '\n')
     echo "Generated with OpenSSL:"
-    ehco "Export like this:"
+    echo "Export like this:"
     echo "export JWT_SECRET=\"$SECRET\""
     echo ""
 fi
 
-if command -v python3 >/dev/null 2>&1; then
-    SECRET=$(python3 -c "import secrets; print(secrets.token_urlsafe(64))")
-    echo "Generated with Python3:"
-    ehco "Export like this:"
-    echo "export JWT_SECRET=\"$SECRET\""
-    echo ""
+if [ -z "$SECRET" ]; then
+  if command -v python3 >/dev/null 2>&1; then
+      SECRET=$(python3 -c "import secrets; print(secrets.token_urlsafe(64))")
+      echo "Generated with Python3:"
+      echo "Export like this:"
+      echo "export JWT_SECRET=\"$SECRET\""
+      echo ""
+  fi
 fi
 
 if [ -z "$SECRET" ]; then 
